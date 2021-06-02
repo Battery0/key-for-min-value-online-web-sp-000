56c3dae71c587ff68d6383c4cc2e4ecdf9ffd33d
Ian King <kickthechair@gmail.com>
2021-06-02 15:24:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,8 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
+      lowest_value = value
+      lowest_key = key
     end
   end
 end
