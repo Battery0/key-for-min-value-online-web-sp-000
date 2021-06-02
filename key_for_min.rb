e0ff4c71ab6e4e402466d2926674dbf855b108ec
Ian King <kickthechair@gmail.com>
2021-06-02 15:17:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
-  name_hash.minmax do |min, max|
-    min
+  name_hash.collect do |key, value|
+    value.min
   end
 end
 
